18da70e51bb287d7f4b09b605df2dc65d2431b53
req-dev <uuserovich33@gmail.com>
2025-05-20 20:24:39 +1000

    Ran Prittier

--- packages/server/eslint.config.js
+++ packages/server/eslint.config.js
@@ -1,24 +1,24 @@
 // packages/server/eslint.config.js
-import eslintPluginTs from '@typescript-eslint/eslint-plugin';
-import parserTs from '@typescript-eslint/parser';
+import eslintPluginTs from "@typescript-eslint/eslint-plugin";
+import parserTs from "@typescript-eslint/parser";
 
 /** @type {import("eslint").Linter.FlatConfig[]} */
 export default [
   {
-    ignores: ['dist/**', 'node_modules/**'],
+    ignores: ["dist/**", "node_modules/**"],
   },
   {
-    files: ['**/*.ts', '**/*.tsx'],
+    files: ["**/*.ts", "**/*.tsx"],
     languageOptions: {
       parser: parserTs,
       parserOptions: {
         ecmaVersion: 2020,
-        sourceType: 'module',
-        project: './tsconfig.json',
+        sourceType: "module",
+        project: "./tsconfig.json",
       },
     },
     plugins: {
-      '@typescript-eslint': eslintPluginTs,
+      "@typescript-eslint": eslintPluginTs,
     },
     rules: {
       ...eslintPluginTs.configs.recommended.rules,
