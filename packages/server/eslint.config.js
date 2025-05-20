b21ee43a344effa249475bdde112b28e72c69159
req-dev <uuserovich33@gmail.com>
2025-05-20 20:21:42 +1000

    added ESlint to the server

---
+++ packages/server/eslint.config.js
@@ -0,0 +1,27 @@
+// packages/server/eslint.config.js
+import eslintPluginTs from '@typescript-eslint/eslint-plugin';
+import parserTs from '@typescript-eslint/parser';
+
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: parserTs,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': eslintPluginTs,
+    },
+    rules: {
+      ...eslintPluginTs.configs.recommended.rules,
+    },
+  },
+];
