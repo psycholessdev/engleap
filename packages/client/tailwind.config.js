9cf8a298587f37b098c57a47f957060f4bd85ca7
req-dev <uuserovich33@gmail.com>
2025-05-21 17:49:16 +1000

    Added client Next.js and set up server

---
+++ packages/client/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  plugins: [],
+};
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,20 +1,47 @@
-import dotenv from "dotenv";
-import cors from "cors";
-dotenv.config();
+import dotenv from "dotenv"
+import cors from "cors"
+import path from "path"
+import express from "express"
+// import your routes here
+import { connectDB } from "./db"
+// import your controllers here
 
-import express from "express";
-import { createClientAndConnect } from "./db";
+// Docker will automatically pass the config in production
+const envConfig =
+  process.env.NODE_ENV === 'development'
+    ? { path: path.resolve(__dirname, '../../../.env') }
+    : undefined
 
-const app = express();
-app.use(cors());
-const port = Number(process.env.SERVER_PORT) || 3001;
+dotenv.config(envConfig)
 
-createClientAndConnect();
+const app = express()
+const PORT = Number(process.env.SERVER_PORT || 3001)
 
-app.get("/", (_, res) => {
-  res.json("👋 Howdy from the server :)");
-});
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === 'development'
+        ? `http://localhost:${process.env.CLIENT_PORT}`
+        : process.env.ORIGIN_PROD_URL,
+    credentials: true,
+  })
+)
+app.use(express.json())
 
-app.listen(port, () => {
-  console.log(`  ➜ 🎸 Server is listening on port: ${port}`);
-});
+// routes here
+// app.use('/api/topics', topicRoutes)
+
+async function startServer() {
+  try {
+    await connectDB()
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`)
+    })
+  } catch (error) {
+    console.error('Failed to connect to database:', error)
+    process.exit(1)
+  }
+}
+
+startServer()
